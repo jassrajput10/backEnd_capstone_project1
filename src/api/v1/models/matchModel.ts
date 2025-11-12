55a80292e7b1a3c89535f21c775a86e87b0d86e6
jparmar10 <jparmar190@rrc.ca>
2025-11-12 04:57:06 -0600

    matches model added

---
+++ src/api/v1/models/matchModel.ts
@@ -0,0 +1,10 @@
+/**
+ * Represents the match in the system
+ */
+export interface matches {
+    id: string,
+    currentGame: string,
+	upcomingMatch: string,
+    location: string,
+    formation: string
+}
