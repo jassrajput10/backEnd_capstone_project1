c784563dd7a61244889e328a3a3a3b742cd2fa10
jparmar10 <jparmar190@rrc.ca>
2025-11-12 05:02:47 -0600

    tournament model added

---
+++ src/api/v1/models/tournamentModel.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents the match in the system
+ */
+export interface tournament {
+    id: string,
+    tournamentName: string,
+    tournamentPosition: string,
+    upcomingTournamnet: string
+}
