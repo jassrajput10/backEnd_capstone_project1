9d2eb5ff1453c7b79c7321fe15fd989cce953c7e
jparmar10 <jparmar190@rrc.ca>
2025-11-15 13:51:00 -0600

    createPlayer controller added

---
+++ src/api/v1/controllers/playerController.ts
@@ -0,0 +1,56 @@
+import { Request, Response, NextFunction } from "express";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
+import * as playerService from "../service/playerService";
+import { player } from "../models/playerModel";
+import { successResponse } from "../models/responseModel";
+
+/**
+ * Manages requests and reponses to retrieve all players
+ * @param req - The express Request
+ * @param res  - The express Response
+ * @param next - The express middleware chaining function
+ */
+export const getAllPlayers = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const players: player[] = await playerService.getAllPlayers();
+        res.status(HTTP_STATUS.OK).json(
+            successResponse(players, "players successfully retireved")
+        );
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+/**
+ * Manages requests, reponses, and validation to create an Player
+ * @param req - The express Request
+ * @param res  - The express Response
+ * @param next - The express middleware chaining function
+ */
+export const createPlayer = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        // Extract only the fields we want (destructuring)
+        // const name: string = req.body.name;
+        // const description: string = req.body.description;
+        const { name, position, jerseyNumber } = req.body;
+
+        const newPlayer: player = await playerService.createPlayer({
+            name,
+            position,
+            jerseyNumber,
+        });
+        res.status(HTTP_STATUS.CREATED).json(
+            successResponse(newPlayer, "player created successfully")
+        );
+    } catch (error: unknown) {
+        next(error);
+    }
+};
